54d4bedb4d9387322088add495ff258da22d2353
ricardoalramos <ricardoalramos@gmail.com>
2020-07-26 19:29:31 -0300

    Materialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className="waves-effect waves-light btn lighten-5">Teste</button>
+      <a class="waves-effect waves-light btn">button</a>
     </div>
   );
 }
